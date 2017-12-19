ecf00c5e6b93c7ea0691c7fdfec3f15e8b31827e
aman <amannanrhe@gmail.com>
2017-12-18 20:34:24 -0700

    Main app setup
    Set up routes and static for html using express

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+var routeIndex = require('./routes/AppController');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routeIndex);
+
+app.listen(5000);
+
+module.exports = app;
